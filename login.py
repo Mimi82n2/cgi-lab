f94d9bfa9d995bf3bffa8ea86f1dd935ffb6164d
Che-Ming <cheming@ualberta.ca>
2022-09-28 14:37:03 -0600

    Login

--- login.py
+++ login.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 
-import os
-import json
 import cgi
 import os
 from re import L
 from templates import login_page
 from templates import secret_page
 
-env = {}
+def parse_cookies(cookie_string):
+    cookies = cookie_string.split(";")
+    result = {}
+    for cookie in cookies:
+        split_cookie = cookie.split("=")
+        result[split_cookie[0]] = split_cookie[1]
+    
+    return result
 
-for env_key, env_value in os.environ.items():
-    env[env_key] = env_value
+#cookies = parse_cookies(os.environ["HTTP_COOKIE"])
 
 form = cgi.FieldStorage()
 
 username = form.getfirst("username")
 password = form.getfirst("password")
+
 header = ""
 header += "Content-Type: text/html\r\n"
+
 body = ""
 #if username is not None or ('logged' in cookies and cookies['logged'] == 'true'):
-if username is not None:
+
+if username is not None #or ('logged' in cookies and cookies['logged'] == 'true'):
     body += secret_page(username, password)
     header += "Set-Cookie: logged=true; Max-Age=60\r\n"
     header += "Set-Cookie: cookie=nom\r\n"
@@ -29,15 +36,6 @@ if username is not None:
 else:
     body += login_page()
 
-print("Content-Type: text/html\n")
-print()
-print("<!doctype html><title>Hello</title><h2>Hello World</h2>")
-print("ENVIRONMENT VAR ", json.dumps(env, sort_keys=True))
-print()
-print("<h2>Query String: {}</h2>".format(os.environ["QUERY_STRING"]))
-print()
-print("<h2>Browser Info: {}</h2>".format(os.environ["HTTP_USER_AGENT"]))
-print()
 print(header)
 print()
 print(body)
